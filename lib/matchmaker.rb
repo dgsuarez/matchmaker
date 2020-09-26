53f3e9237e5bd71fdf640c0aa97bf4590752bf27
Diego Guerra <diego.guerra.suarez@gmail.com>
2020-09-26 15:12:41 +0200

    Simpler score with matching

--- lib/matchmaker.rb
+++ lib/matchmaker.rb
@@ -3,31 +3,58 @@ require "matchmaker/version"
 module Matchmaker
   class Error < StandardError; end
 
-  def self.generate_match(preferences, discriminator: Randon.new)
-    matches = {}
-
-    preferences.values.flatten.each do |current_group|
-      scores = preferences.transform_values {|groups| groups.index(current_group) || 1000 }
-
-      until matches[current_group]
-        best_participant, best_participant_score = scores
-          .reject { |participant,| matches.values.include?(participant) }
-          .sort_by {|_, score| [score, discriminator.rand] }
-          .first
-
-        existing_participant = matches[current_group]
-
-        if existing_participant
-          existing_participant_score = scores[existing_participant]
-          if best_participant_score < existing_participant_score
-            matches[current_group] = best_participant
-          end
-        else
-          matches[current_group] = best_participant
-        end
-      end
+  class Match
+    def initialize(preferences, discriminator: Random.new)
+      @preferences = preferences
+      @discriminator = discriminator
+      @matches = nil
     end
 
-    matches.map { |key, value| [value, key] }.to_h
+    def match
+      return matches if matches
+      self.matches = {}
+
+      groups.each do |current_group|
+        best_participant, = preferences.
+          transform_values {|groups| [groups.index(current_group) || missing_group_score, discriminator.rand] }
+          .sort_by { |_, group_score| group_score }
+          .reject { |participant,| matches[participant] }
+          .first
+
+        matches[best_participant] = current_group
+      end
+
+      matches
+    end
+
+    def score
+      individual_scores = match.map { |participant, group| preferences[participant].index(group) }
+
+      [individual_scores.reduce(&:+), variance(individual_scores)]
+    end
+
+    private
+
+    attr_accessor :matches
+    attr_reader :preferences, :discriminator
+
+    def groups
+      @groups ||= preferences.values.flatten.uniq
+    end
+
+    def missing_group_score
+      groups.length + 1
+    end
+
+    def variance(scores)
+      mean_score = scores.reduce(&:+) / scores.length.to_f
+      squared_diff_sum = scores.map { |score| (score - mean_score)**2 }.reduce(&:+)
+      squared_diff_sum/scores.size
+    end
+
+  end
+
+  def self.generate_match(preferences, discriminator:)
+    Match.new(preferences, discriminator: discriminator).match
   end
 end
--- spec/matchmaker_spec.rb
+++ spec/matchmaker_spec.rb
@@ -1,19 +1,46 @@
 RSpec.describe Matchmaker do
-  it 'matches preferences' do
-    preferences = {
-      person_1: [:group_a, :group_b, :group_c],
-      person_2: [:group_c, :group_a],
-      person_3: [:group_a, :group_c, :group_b],
-    }
+  describe Matchmaker::Match do
+    let(:preferences) do
+      {
+        participant_1: [:group_a, :group_b, :group_c],
+        participant_2: [:group_c, :group_a],
+        participant_3: [:group_a, :group_c, :group_b],
+      }
+    end
 
-    expected = {
-      person_1: :group_b,
-      person_2: :group_c,
-      person_3: :group_a
-    }
+    let(:discriminator) { Random.new(1) }
+    subject(:single_match) { described_class.new(preferences, discriminator: discriminator) }
 
-    actual = Matchmaker.generate_match(preferences, discriminator: Random.new(1))
+    it 'matches preferences' do
+      expected = {
+        participant_1: :group_b,
+        participant_2: :group_c,
+        participant_3: :group_a
+      }
 
-    expect(actual).to eq(expected)
+      expect(single_match.match).to eq(expected)
+    end
+
+    it 'knows its match score' do
+      expect(single_match.score).to match([1, be_within(0.1).of(0.22)])
+    end
+
+    context 'with a different discriminator' do
+      let(:discriminator) { Random.new(321) }
+
+      it 'resolves ties differently' do
+        expected = {
+          participant_1: :group_a,
+          participant_2: :group_c,
+          participant_3: :group_b
+        }
+
+        expect(single_match.match).to eq(expected)
+      end
+
+      it 'has a differenty match score' do
+        expect(single_match.score).to match([2, be_within(0.1).of(0.88)])
+      end
+    end
   end
 end
