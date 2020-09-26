3e034ba089ce438cb642fc7d1325ba7a754bc753
Diego Guerra <diego.guerra.suarez@gmail.com>
2020-09-26 14:03:33 +0200

    Basic implementation

--- lib/matchmaker.rb
+++ lib/matchmaker.rb
@@ -2,5 +2,32 @@ require "matchmaker/version"
 
 module Matchmaker
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.generate_match(preferences, discriminator: Randon.new)
+    matches = {}
+
+    preferences.values.flatten.each do |current_group|
+      scores = preferences.transform_values {|groups| groups.index(current_group) || 1000 }
+
+      until matches[current_group]
+        best_participant, best_participant_score = scores
+          .reject { |participant,| matches.values.include?(participant) }
+          .sort_by {|_, score| [score, discriminator.rand] }
+          .first
+
+        existing_participant = matches[current_group]
+
+        if existing_participant
+          existing_participant_score = scores[existing_participant]
+          if best_participant_score < existing_participant_score
+            matches[current_group] = best_participant
+          end
+        else
+          matches[current_group] = best_participant
+        end
+      end
+    end
+
+    matches.map { |key, value| [value, key] }.to_h
+  end
 end
--- spec/matchmaker_spec.rb
+++ spec/matchmaker_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe Matchmaker do
-  it "has a version number" do
-    expect(Matchmaker::VERSION).not_to be nil
-  end
+  it 'matches preferences' do
+    preferences = {
+      person_1: [:group_a, :group_b, :group_c],
+      person_2: [:group_c, :group_a],
+      person_3: [:group_a, :group_c, :group_b],
+    }
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    expected = {
+      person_1: :group_b,
+      person_2: :group_c,
+      person_3: :group_a
+    }
+
+    actual = Matchmaker.generate_match(preferences, discriminator: Random.new(1))
+
+    expect(actual).to eq(expected)
   end
 end
